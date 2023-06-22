c906268084a14b29998e5bdcdf1b8d5e37886deb
Abdurrazzak Saymaz <abdurrazzak1905@gmail.com>
2023-06-22 14:37:20 +0300

    updated

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","0","1","2","3","4","5","6","7","8","9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
+
 
 
 const generatePassword = document.getElementById("generater")
-
+let getLength = document.getElementById("getLength")
 
 generatePassword.addEventListener("click" ,()=> {
     
@@ -18,8 +18,9 @@ if(getLength.value < 17){
     password1.textContent = key1
     password2.textContent = key2
     }else{
-        alert("input value must be lower then 16")
-    }
+        alert("INPUT VALUE MUST BE LOWER THEN 16")
+        
+}})
     password1.addEventListener("click", ()=>{
         document.execCommand("copy");
     })
@@ -28,7 +29,6 @@ if(getLength.value < 17){
     event.preventDefault();
     if (event.clipboardData) {
     event.clipboardData.setData("text/plain", password1.textContent);
-    console.log("copied")
     }})
      password2.addEventListener("click", ()=>{
         document.execCommand("copy");
@@ -38,7 +38,5 @@ if(getLength.value < 17){
     event.preventDefault();
     if (event.clipboardData) {
     event.clipboardData.setData("text/plain", password2.textContent);
-    console.log("copied")
     }});
-})
 
